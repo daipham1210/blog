c065a9a53084135c34d1ca4bca18bc757c3cef81
daipham <daipham1210@gmail.com>
2020-01-07 16:29:24 +0700

    update default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_controller.default_url_options = { host: 'daipham.xyz' }
   # config.action_cable.allowed_request_origins = []
   # config.action_cable.url = ""
 end
