8f6b5049cfff90423091b3a035ed3aec7004f31c
daipham <daipham1210@gmail.com>
2020-01-07 11:46:32 +0700

    last fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+         :recoverable, :rememberable, :trackable, :validatable
 
 
   validates_presence_of :name
