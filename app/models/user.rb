35b7119f81ebcd237922f39d0e6fe20e79aa72f8
daipham <daipham1210@gmail.com>
2020-01-07 11:12:39 +0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
+
 
   validates_presence_of :name
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :topics, only: [:index, :show]
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '', controllers: { omniauth_callbacks: "callbacks" },
+                     path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  devise_scope :user do
-    get '/auth/github/callback', to: 'callbacks#github'
-  end
+  # devise_scope :user do
+  #   get '/auth/github/callback', to: 'callbacks#github'
+  # end
 
   root 'blogs#index'
   get 'about-me', to: 'pages#about'
