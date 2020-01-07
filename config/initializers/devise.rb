4b5ea54827a5d74b2f954e291c04dbee8eb3e81d
daipham <daipham1210@gmail.com>
2020-01-07 11:25:29 +0700

    test

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -246,7 +246,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['GITHUB_KEY'],  ENV['GITHUB_SECRET'],  scope: "user:email"
+  config.omniauth :github, ENV['GITHUB_KEY'],  ENV['GITHUB_SECRET'], { redirect_uri: 'http://daipham.xyz', scope: "user:email" }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
