8f393b7a31621e610ca487b28f46a8e010e1f77c
daipham <daipham1210@gmail.com>
2020-01-07 11:21:28 +0700

    test

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    Rails.logger.info @user.email
     sign_in_and_redirect @user
   end
 end
