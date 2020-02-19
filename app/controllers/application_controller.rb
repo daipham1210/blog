f3b466a2c92fa3a5c08ac4c927c9608979104ec5
daipham1210 <daipham1210@gmail.com>
2020-02-19 15:35:33 +0700

    update devise redirect back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def store_user_location!
     store_location_for(:user, request.fullpath)
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
 end
